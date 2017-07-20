e65ced0de8c61009120b5c992d9c38d7234e0746
Mac User <sabbey37@gmail.com>
2017-07-20 16:55:52 -0400

    Store variables locally

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,33 @@
 var $coffeeForm = $('[data-coffee-order="form"]');
+var $order = $('[data-role="order"]');
+var $email = $('[data-role="email"]');
+
+
+function storeValue(name, title) {
+    localStorage.setItem(title, name.val());
+}
+
+function getSize() {
+    var size = $('input[data-role=size]:checked').val();
+    localStorage.setItem('size', size);
+}
+
+function getFlavor() {
+    var flavor = $('[data-role="flavor"]').find(":selected").text();
+    localStorage.setItem('flavor', flavor);
+}
+
+function getStrength() {
+    var strength = $('[data-role="strength"]').val();
+    localStorage.setItem('strength', strength);
+}
+
+
 $coffeeForm.submit(function (event) {
-  // do something
+  event.preventDefault();
+  storeValue($order, 'order');
+  storeValue($email, 'email');
+  getSize();
+  getFlavor();
+  getStrength();
 });
