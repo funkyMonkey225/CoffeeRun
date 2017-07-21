ddef46bdbcd494a9862a16a7e234228b496c2226
Mac User <sabbey37@gmail.com>
2017-07-21 15:07:19 -0400

    Add functions to get/post data to API

--- scripts/main2.js
+++ scripts/main2.js
@@ -2,46 +2,66 @@
 var $coffeeForm = $('[data-coffee-order="form"]');
 var $order = $('[data-role="order"]');
 var $email = $('[data-role="email"]');
-var URL = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders';
-var orders = {};
+var $size = $('[data-role="size"]')
+var $flavor = $('[data-role="flavor"]');
+var $strength = $('[data-role="strength"]');
 
-function storeValue(name, title) {
-    localStorage.setItem(title, name.val());
+var URL = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders';
+var order = {};
+var priorOrders;
+
+function storeValue(name) {
+    order[name.attr('name')] = name.val();
 }
 
 function getSize() {
     var size = $('input[data-role=size]:checked').val();
-    localStorage.setItem('size', size);
+    order[$size.attr('name')] = size;
 }
 
 function getFlavor() {
-    var flavor = $('[data-role="flavor"]').find(":selected").text();
-    localStorage.setItem('flavor', flavor);
+    var flavor = $flavor.find(":selected").text();
+    order[$flavor.attr('name')] = flavor;
 }
 
 function getStrength() {
-    var strength = $('[data-role="strength"]').val();
-    localStorage.setItem('strength', strength);
+    var strength = $strength.val();
+    order[$strength.attr('name')] = strength;
 }
 
 // 
-function setDefaults() {
-    var flavorValue = (localStorage.getItem('flavor')).toLowerCase();
-    $order.val(localStorage.getItem('order'));
-    $email.val(localStorage.getItem('email'));
-    $('input[data-role=size]:checked').val('size');
-    $('input:radio[data-role=size]').filter('[value=' + localStorage.getItem('size') + ']').prop('checked', true);
-    $('[data-role="flavor"]').find('option[value=' + flavorValue + ']').prop('selected', true);
-    $('[data-role="strength"]').val(localStorage.getItem('strength'));
+// function setDefaults() {
+//     var flavorValue = (localStorage.getItem('flavor')).toLowerCase();
+//     $order.val(localStorage.getItem('order'));
+//     $email.val(localStorage.getItem('email'));
+//     $('input[data-role=size]:checked').val('size');
+//     $('input:radio[data-role=size]').filter('[value=' + localStorage.getItem('size') + ']').prop('checked', true);
+//     $flavor.find('option[value=' + flavorValue + ']').prop('selected', true);
+//     $('[data-role="strength"]').val(localStorage.getItem('strength'));
+// }
+
+function getServerData() {
+    $.get(URL, storeData);
+} 
+    
+function storeData(orders) {
+    localStorage.setItem('priorOrders', JSON.stringify(orders));
 }
+
+function sendDataToServer() {
+    $.post(URL, order, function (resp) {
+        console.log(resp);
+    });
+}
+
+// setDefaults();
 
 $coffeeForm.submit(function (event) {
   event.preventDefault();
-  storeValue($order, 'order');
-  storeValue($email, 'email');
+  storeValue($order);
+  storeValue($email);
   getSize();
   getFlavor();
   getStrength();
+  sendDataToServer();
 });
-
-setDefaults();
